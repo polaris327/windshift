b3f4373f1429d21f3bde24f28d45236718481490
polaris327 <webstar317@yahoo.com>
2018-08-26 23:58:32 +0800

    fix: put the quote page back

--- src/app/pages/quote-page/quote-page-routing.module.ts
+++ src/app/pages/quote-page/quote-page-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { QuotePageComponent } from './quote-page.component';
 
 const routes: Routes = [
-  // {path: '', component: QuotePageComponent}
+  {path: '', component: QuotePageComponent}
 ];
 
 @NgModule({
